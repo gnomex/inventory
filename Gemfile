df060c702d3d2a755744a52cc976bacc7a56597d
Kenner Kliemann <kenner.hp@gmail.com>
2015-07-13 23:03:16 -0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,36 @@
+source 'https://rubygems.org'
+
+ruby '2.2.1'
+
+gem 'rails', '4.2.3'
+gem 'rails-i18n', '~> 4.0.3'
+gem 'pg'
+gem 'responders', '~> 2.1.0'
+gem 'bcrypt', '~> 3.1.7'
+
+group :assets do
+  gem 'sass-rails', '~> 5.0'
+  gem 'uglifier', '>= 1.3.0'
+  gem 'coffee-rails', '~> 4.1.0'
+  gem 'turbolinks'
+  gem "therubyracer", "~> 0.12.1"
+end
+
+group :development, :test do
+  gem 'byebug'
+  gem 'web-console', '~> 2.0'
+  gem 'spring'
+  gem 'pry-rails'
+  gem 'pry-rescue'
+  gem 'rspec-rails', '~> 3.1.0'
+  gem 'rspec-collection_matchers'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'launchy'
+  gem 'selenium-webdriver'
+end
+
+gem 'sdoc', '~> 0.4.0', group: :doc
---
+++ config/application.rb
@@ -0,0 +1,50 @@
+require File.expand_path('../boot', __FILE__)
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module CeltabInventory
+  class Application < Rails::Application
+    # Use the responders controller from the responders gem
+    config.app_generators.scaffold_controller :responders_controller
+
+    config.time_zone = 'Brasilia'
+
+    config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.yml").to_s]
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = [:en, 'pt-BR', :es, :de]
+    config.i18n.enforce_available_locales = false
+    config.encoding = "utf-8"
+
+    # config.active_record.whitelist_attributes = true
+    config.action_controller.action_on_unpermitted_parameters = :raise
+
+    # To re-enable this authenticity token for remote forms
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+
+    # Disable the asset pipeline.
+    config.assets.enabled = false
+    # config.assets.css_compressor = :yui
+    config.assets.css_compressor = :sass
+    config.assets.js_compressor = :uglify
+
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower')
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
+    # config.web_console.whitelisted_ips = '192.168.0.150/24'
+    config.web_console.whitelisted_ips = '127.0.1.1'
+    config.web_console.whiny_requests = false
+  end
+end
