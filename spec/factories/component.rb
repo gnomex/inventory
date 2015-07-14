897e89ce9ae809f0210e109d87a0eed091f89499
Kenner Kliemann <kenner.hp@gmail.com>
2015-07-14 19:58:16 -0300

    More tests for components

--- spec/factories/component.rb
+++ spec/factories/component.rb
@@ -3,6 +3,8 @@ FactoryGirl.define do
     part_number "abcd123"
     stock 10
     description "Something mocked"
+    manufacturer "gnomex"
+    image_link "someplace.io/image.png"
     datasheet "www.alldatasheets.com/search?q=abcd1234"
   end
 end
--- spec/models/component_spec.rb
+++ spec/models/component_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Component, :type => :model do
   context "Required fields" do
-    it "don't create with invalid data" do
+    it "don't create with blank or invalid data" do
       component = Component.new
       expect(component.errors_on(:part_number)).to_not be_empty
       expect(component.errors_on(:stock)).to_not be_empty
@@ -12,40 +12,37 @@ RSpec.describe Component, :type => :model do
     end
   end
 
-  context "Model rules" do
+  context "Model validations" do
+    before(:each) do
+      @component = build(:component, status: { done: "nooop" })
+    end
+
     it "doesn't trow exception" do
-      # expect {
-      #   Component.create!({
-      #     part_number: "abcd12345",
-      #     stock: 1,
-      #     description: "A example of component",
-      #     datasheet: "www.alldatasheets.com/abcd12345",
-      #     status: {in_use: "noop"}
-      # })}.to_not raise_error
-      component = build(:component, status: { done: "nooop" })
-      expect { component.save! }.to_not raise_error
+      expect { @component.save! }.to_not raise_error
+    end
+
+    it "Part number can't be blank" do
+      @component.part_number = ""
+      expect(@component).to_not be_valid
     end
 
     it "have a normalized Part Number" do
-      component = build(:component, status: { done: "yeap" })
-      component.save!
+      @component.save!
 
-      expect(component.part_number).to eql("ABCD123")
+      expect(@component.part_number).to eql("ABCD123")
     end
   end
 
-  context "Unique data" do
+  context "Unique" do
     before(:all) do
       @component = build(:component, status: { testing: "yep" })
     end
 
-    it "part number" do
+    it "Part number" do
       @component.save!
 
       cm = build(:component, status: { testing: "yep" })
       cm.save!
-
-      expect(cm.errors_on(:part_number)).to_not be_empty
     end
   end
 end
