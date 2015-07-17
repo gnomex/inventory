34ab2adee105a78b736c9327f1fcf24edb9533e7
Kenner Kliemann <kenner.hp@gmail.com>
2015-07-17 17:53:34 -0300

    The toolbox feature specs

---
+++ spec/features/toolboxes_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "Toolboxes features", type: :feature, js: true do
+  scenario 'create with valid data' do
+    visit '/toolboxes/new'
+
+    name = Faker::App.name
+
+    within("#new_toolbox") do
+      fill_in 'toolbox_name', with: name
+      fill_in 'toolbox_owner', with: Faker::Name.name
+      fill_in 'toolbox_description', with: Faker::Hacker.say_something_smart
+    end
+
+    click_on "Save!"
+
+    expect(page.current_path).to eql(toolboxes_path)
+    expect(page).to have_content "The sandbox #{name.camelize} has been created!"
+  end
+
+  # scenario "don't create and raise errors with invalid data" do
+  # end
+
+  # scenario 'assign a category' do
+  #   # find('#component_category_id').click
+  # end
+
+  # scenario 'remove' do
+  # end
+end
