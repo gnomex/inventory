e256bc6a4ac4618a04adb0826b25c3bdf73aa5c9
Kenner Kliemann <kenner.hp@gmail.com>
2015-07-16 23:41:46 -0300

    Change the cookie store name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_celtab-inventory_session'
+Rails.application.config.session_store :cookie_store, key: 'c_09fb82f-df060c7'
